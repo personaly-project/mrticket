5673ece4399f2fd826bd7ab26b9d99005203085e
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-02-10 16:12:35 +0100

    function to filter array of objects

---
+++ src/components/Layout/Search.tsx
@@ -0,0 +1,49 @@
+/** @format */
+import { useEffect, useState } from "react";
+import filterObject from "@/lib/filterObject";
+import React from "react";
+
+interface IProps {
+  ticketSearch: string;
+  setTicketSearch: (ticketSearch: string) => void;
+}
+
+export default function Search({ ticketSearch, setTicketSearch }: IProps) {
+  const [venues, setVenues] = useState<any>([]);
+  const handleChange = (e: any) => {
+    setTicketSearch(e.target.value);
+    // console.log keys from venues
+    // get all the keys from an object
+    console.log(filterObject(venues.venue, venues.venue[0], ticketSearch));
+  };
+
+  useEffect(() => {
+    const venuesLocally = fetch("http://localhost:3000/api/getallvenues")
+      .then((res) => res.json())
+      .then((data) => {
+        setVenues(data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      Find your Ticket
+      <input
+        style={{
+          width: "auto",
+          height: "100%",
+          backgroundColor: "transparent",
+          border: "none",
+          outline: "none",
+          marginLeft: "10px",
+          color: "black",
+        }}
+        type="text"
+        value={ticketSearch}
+        onChange={(e) => handleChange(e)}
+        placeholder="Enter your search here"
+      />
+    </div>
+  );
+}

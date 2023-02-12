7f0e97ad3f22647192be08259bda5aef4edbf8af
Imagine96 <rubged9606@gmail.com>
2023-02-12 17:33:21 +0100

    withBearerToken enforcement middleware

--- src/lib/middleware/withAuth.ts
+++ src/lib/middleware/withAuth.ts
@@ -9,7 +9,25 @@ export const enforceBearerToken = (handler: NextApiHandler) => {
         if (!userId || typeof userId !== "string" || !authorizationHeader) return res.status(401).json({ error: "missing authentication in request" })
         try {
             const decoded = decode<IUser>(authorizationHeader)
+
             if (decoded.id !== userId) return res.status(403).json({ error: "forbidden" })
+
+            const newToken = encode(decoded)
+            res.setHeader("Refreshed_Token", newToken)
+            return handler(req, res)
+        } catch (err) {
+            res.status(401).json({ error: "invalid token" })
+        }
+    }
+}
+
+export const withBearerToken = (handler: NextApiHandler) => {
+    return async (req: NextApiRequest, res: NextApiResponse<IApiResponse<unknown>>) => {
+        const { userId } = req.query
+        const authorizationHeader = req.headers.authorization
+        if (!userId || typeof userId !== "string" || !authorizationHeader) return res.status(401).json({ error: "missing authentication in request" })
+        try {
+            const decoded = decode<IUser>(authorizationHeader)
             const newToken = encode(decoded)
             res.setHeader("Refreshed_Token", newToken)
             return handler(req, res)

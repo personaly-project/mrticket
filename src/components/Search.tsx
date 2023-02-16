7dbf192490168ec697799b1ac8fa96ae38f7e7f6
Martina Cumellas <cumellasmartina@gmail.com>
2023-02-16 12:46:47 +0100

    solved conflicts

---
+++ src/components/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Search({}: Props) {
+  return (
+    <div className="bg-[#a7a7a7]  h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden bg-center bg-cover">
+      <h1 className="text-3xl font-anekbangla uppercase text-[#fb8500]">
+        {" "}
+        search bar
+      </h1>
+    </div>
+  );
+}
--- src/lib/context/Auth/authContext.tsx
+++ src/lib/context/Auth/authContext.tsx
@@ -3,100 +3,108 @@ import { createContext, ReactNode, useCallback, useState } from "react";
 import { IApiResponse, INewUserSrcData, IUser } from "../../types";
 
 interface IAuthContext {
-    user?: IUser,
-    error?: string
-    isLoading: boolean,
-    login: (email: string, psw: string) => void,
-    signUp: (src: INewUserSrcData) => void,
-    logout: () => void,
+  user?: IUser;
+  error?: string;
+  isLoading: boolean;
+  login: (email: string, psw: string) => void;
+  signUp: (src: INewUserSrcData) => void;
+  logout: () => void;
 }
 
 const defaultCtx: IAuthContext = {
-    login: (email: string, psw: string) => { },
-    signUp: (src: INewUserSrcData) => { },
-    logout: () => { },
-    isLoading: false
-}
+  login: (email: string, psw: string) => {},
+  signUp: (src: INewUserSrcData) => {},
+  logout: () => {},
+  isLoading: false,
+};
 
-export const authCtx = createContext<IAuthContext>(defaultCtx)
+export const authCtx = createContext<IAuthContext>(defaultCtx);
 
 interface IProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 export const AuthContextProvider: React.FC<IProps> = ({ children }) => {
-    const [user, setUser] = useState<IUser | undefined>()
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-    const [error, setError] = useState<string>()
-    const router = useRouter()
+  const [user, setUser] = useState<IUser | undefined>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+  const router = useRouter();
 
-    const login = useCallback(async (email: string, psw: string) => {
-        setIsLoading(true)
-        const resp = await fetch('/api/login', {
-            method: "POST",
-            headers: {
-                accept: "application/json",
-            },
-            body: JSON.stringify({ email, psw })
-        })
-        const { data, error } = await resp.json() as IApiResponse<IUser>
+  const login = useCallback(
+    async (email: string, psw: string) => {
+      setIsLoading(true);
+      const resp = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+        },
+        body: JSON.stringify({ email, psw }),
+      });
+      const { data, error } = (await resp.json()) as IApiResponse<IUser>;
+      console.log(data);
+      if (error) {
+        //server defined error
+        setError(error);
+      } else if (data) {
+        //success
+        setUser(data);
+        router.push("/");
+      } else {
+        //undefined error not carried trough the error field en the api response
+        setError("unknown error");
+      }
+      setIsLoading(false);
+    },
+    [router]
+  );
 
-        if (error) {
-            //server defined error
-            setError(error)
-        } else if (data) {
-            //success
-            setUser(data)
-            router.push('/')
-        } else {
-            //undefined error not carried trough the error field en the api response
-            setError("unknown error")
-        }
-        setIsLoading(false)
-    }, [router])
+  const signUp = useCallback(
+    async (src: INewUserSrcData) => {
+      setIsLoading(true);
+      const resp = await fetch("/api/signUp", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+        },
+        body: JSON.stringify(src),
+      });
 
-    const signUp = useCallback(async (src: INewUserSrcData) => {
-        setIsLoading(true)
-        const resp = await fetch('/api/signUp', {
-            method: "POST",
-            headers: {
-                accept: "application/json",
-            },
-            body: JSON.stringify(src)
-        })
+      const { data, error } = (await resp.json()) as IApiResponse<IUser>;
 
-        const { data, error } = await resp.json() as IApiResponse<IUser>
+      if (error) {
+        //server defined error
+        setError(error);
+      } else if (data) {
+        //success
+        setUser(data);
+        router.push("/");
+      } else {
+        //undefined error not carried trough the error field en the api response
+        setError("unknown error");
+      }
+      setIsLoading(false);
+    },
+    [router]
+  );
 
-        if (error) {
-            //server defined error
-            setError(error)
-        } else if (data) {
-            //success
-            setUser(data)
-            router.push('/')
-        } else {
-            //undefined error not carried trough the error field en the api response
-            setError("unknown error")
-        }
-        setIsLoading(false)
-    }, [router])
+  const logout = useCallback(() => {
+    setUser(undefined);
+    setError(undefined);
+    setIsLoading(false);
+  }, []);
 
-    const logout = useCallback(() => {
-        setUser(undefined)
-        setError(undefined)
-        setIsLoading(false)
-    }, [])
-
-    return (
-        <authCtx.Provider value={{
-            login,
-            logout,
-            signUp,
-            isLoading,
-            error,
-            user
-        }} >
-            {children}
-        </authCtx.Provider>
-    )
-}
+  return (
+    <authCtx.Provider
+      value={{
+        login,
+        logout,
+        signUp,
+        isLoading,
+        error,
+        user,
+      }}
+    >
+      {children}
+    </authCtx.Provider>
+  );
+};

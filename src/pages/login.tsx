450ff1a678097d1c4081a5002c92646910c648bc
Martina Cumellas <cumellasmartina@gmail.com>
2023-02-18 11:03:17 +0100

    fix: import image

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,35 +1,43 @@
-import React, { useContext, useState } from 'react'
-import { Login, SignUp } from '@/components/Auth'
-import { authCtx } from '@/lib/context/Auth/authContext'
-import Image from 'next/image'
-import logo from "../components/images/logoWordsPurple.png"
+import React, { useContext, useState } from "react";
+import { Login, SignUp } from "@/components/Auth";
+import { authCtx } from "@/lib/context/Auth/authContext";
+import Image from "next/image";
+import logo from "../../public/logoWordsPurple.png";
 
 const LoginPage = () => {
+  const { login, signUp, isLoading } = useContext(authCtx);
+  const [register, setRegister] = useState<boolean>(false);
 
-    const { login, signUp, isLoading } = useContext(authCtx)
-    const [register, setRegister] = useState<boolean>(false)
+  const toggleRegister = () => {
+    setRegister((prev) => !prev);
+  };
 
-    const toggleRegister = () => {
-        setRegister(prev => !prev)
-    }
-
-    if (isLoading) return <div className='min-h-screen flex items-center justify-center'> Loading </div>
-
+  if (isLoading)
     return (
-        <div className='min-h-screen flex items-center justify-center' >
-            <div className='shadow flex flex-col gap-4 p-8 rounded relative' >
-                {
-                    register ? <SignUp dispatchRegister={signUp} /> : <Login dispatchLogin={login} />
-                }
-                <sub onClick={toggleRegister} className='cursor-pointer underline opacity-80' >
-                    {
-                        register ? "Have an account? Login!" : "New here? Register!"
-                    }
-                </sub>
-                <Image className='-z-50 opacity-10' src={logo} alt='' fill />
-            </div>
-        </div>
-    )
-}
+      <div className="min-h-screen flex items-center justify-center">
+        {" "}
+        Loading{" "}
+      </div>
+    );
 
-export default LoginPage
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="shadow flex flex-col gap-4 p-8 rounded relative">
+        {register ? (
+          <SignUp dispatchRegister={signUp} />
+        ) : (
+          <Login dispatchLogin={login} />
+        )}
+        <sub
+          onClick={toggleRegister}
+          className="cursor-pointer underline opacity-80"
+        >
+          {register ? "Have an account? Login!" : "New here? Register!"}
+        </sub>
+        <Image className="-z-50 opacity-10" src={logo} alt="" fill />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;

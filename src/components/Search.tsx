381eaa47d9d90010da488f1ec87d4a7ee74aee1e
Martina Cumellas <cumellasmartina@gmail.com>
2023-02-18 09:42:03 +0100

    feat: navbar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Search({}: Props) {
+export default function Search() {
   return (
     <div className="bg-[#a7a7a7]  h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden bg-center bg-cover">
       <h1 className="text-3xl font-anekbangla uppercase text-[#fb8500]">

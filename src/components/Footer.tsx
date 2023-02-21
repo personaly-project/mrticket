2a938d43ae0e0efd21fe8737b55c17c363ffc2e6
Martina Cumellas <cumellasmartina@gmail.com>
2023-02-21 11:12:51 +0100

    style: payment page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import crowd from "./images/crowd2.jpg";
 
 export default function Hero() {
   return (
-    <footer className="flex justify-center items-center shadow-inner">
+    <footer className="flex justify-center items-center shadow-inner pt-8 pb-2 text-purple-medium text-xs bg-purple-dark">
       <p>Copyright © 2023 Mister Ticket </p>
     </footer>
   );
